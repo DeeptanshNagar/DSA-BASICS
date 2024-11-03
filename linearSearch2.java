da837235469f2769730369098b37938ca5cd4294
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-11-04 01:49:45 +0530

    Linear Search 2

--- linearSearch2.java
+++ linearSearch2.java
@@ -6,6 +6,28 @@ public class linearSearch2 {
         System.out.println(ans);
     }
 
+    /*
+        Search in the array: return the index if item found
+        otherwise if item not found return -1
+     */
+    static int linearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        // Run a for loop
+        for (int index = 0; index < arr.length; index++) {
+            // Check for element at every index if it is equal to target
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+        }
+
+        // this line will execute if none of the return statements above have executed, hence the target not found 
+        return -1;
+    }
+
     // search the target and return true or false
     static boolean linearSearch3(int[] arr, int target) {
         if (arr.length == 0) {
@@ -39,28 +61,4 @@ public class linearSearch2 {
         // this line will execute if none of the return statements above have executed, hence the target not found 
         return Integer.MAX_VALUE;
     }
-
-
-
-    /*
-        Search in the array: return the index if item found
-        otherwise if item not found return -1
-     */
-    // static int linearSearch(int[] arr, int target) {
-    //     if (arr.length == 0) {
-    //         return -1;
-    //     }
-
-    //     // Run a for loop
-    //     for (int index = 0; index < arr.length; index++) {
-    //         // Check for element at every index if it is equal to target
-    //         int element = arr[index];
-    //         if (element == target) {
-    //             return index;
-    //         }
-    //     }
-
-    //     // this line will execute if none of the return statements above have executed, hence the target not found 
-    //     return -1;
-    // }
 }
