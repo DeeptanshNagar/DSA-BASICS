d4efe179dac6ab1a94398ac2604a487c02d0b262
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-18 00:20:21 +0530

    dsa.lc

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,17 @@
+nt digits(int num) {
+
+    //     if (num < 0) {
+    //         num = num * -1;
+    //     }
+
+    //     if (num == 0) {
+    //         return 1;
+    //     }
+
+    //     int count = 0;
+    //     while (num > 0) {
+    //         count++;
+    //         num = num / 10;    // num /= 10
+    //     }
+    //     return count;
+    // }
