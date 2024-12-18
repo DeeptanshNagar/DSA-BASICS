5741c4e16a801e2794edeec91c1885ce71ecf13e
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-18 16:22:27 +0530

    dsa.kk

--- Greeting.java
+++ Greeting.java
@@ -1,5 +1,8 @@
 public class Greeting {
     public static void main(String[] args) {
-            
+        greeting();
+    }
+    static void greeting() {                               // when you want to return nothing from the function body we use void.
+        System.out.println("Hello World");
     }
 }
--- sum.java
+++ sum.java
@@ -5,8 +5,19 @@ public class sum {
         sum();
     }
 
+    // return the value
+    static int sum2() {               // when you want to return a integer or any other dataType from the function body we use int or other dataTypes in func defination.
+        Scanner in = new Scanner(System.in); 
+        int num1, num2, sum;
+        System.out.println("Enter number 1: ");
+        num1 = in.nextInt();
+        System.out.println("Enter number 2: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        return sum;
+    }
 
-    static void sum () {
+    static void sum () {                                      // when you want to return nothing from the function body we use void in function defination.
         Scanner in = new Scanner(System.in);
         int num1, num2, sum;
         System.out.println("Enter number 1: ");
