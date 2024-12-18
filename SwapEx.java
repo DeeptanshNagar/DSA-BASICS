d8f440bfb84702a03d2e637dcfa3a088632fd7af
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-19 00:44:01 +0530

    swap

--- SwapEx.java
+++ SwapEx.java
@@ -11,12 +11,13 @@ public class SwapEx {
         swap(a, b);
         System.out.println(a + " " + b);
 
-        String name = "Kunal Kushwaha";
-        changeName(name);
+        String name = "Kunal Kushwaha";        // name is reference variable. 
+        // copy of the value of reference variable is passed in next step.
+        changeName(name);                     // value of reference variable is passed.
         System.out.println(name);
     }
 
-    static void changeName(String naam) {
+    static void changeName(String naam) {     // naam is the scope of the function means naam is only accessible in the function body only.
         naam = "Rahul Rana";                 // not changing, creating a new object rahul rana and naam is pointing to that object.
     }
 
