2be03fff0b2b102f4696fe9b1fac9155df7f9e7a
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-05-25 23:45:08 +0530

    arr

--- input.java
+++ input.java
@@ -34,7 +34,7 @@ public class input {
         String[] str = new String[4];
         for (int i = 0; i < str.length; i++){
             str[i] = in.next();
-        }
+        } 
 
         System.out.println(Arrays.toString(str));
 
