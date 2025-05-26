27c95c7190a595be85dce1fe20be94d30a7d580b
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-05-26 17:32:40 +0530

    dsa

--- MultiDimensionArray.java
+++ MultiDimensionArray.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class MultiDimensionArray {
     public static void main(String[] args) {
@@ -17,41 +15,41 @@ public class MultiDimensionArray {
         //     {1, 2, 3},    // 0
         //     {4, 5, 6},    // 1
         //     {7, 8, 9}     // 2
-        //     // Imagine this as an array of arrays.
+        //     // Imagine this is as an array of arrays.
         // };
 
-        Scanner in = new Scanner(System.in);
+        // Scanner in = new Scanner(System.in);
 
         // int[][] arr = new int[3][];
 
-        // int[][] arr = {
-        //     {1, 2, 3},       // 0th index
-        //     {4, 5},          // 1st index
-        //     {6, 7, 8, 9}     // 2nd index  -> arr2D[2] = {6, 7, 8, 9}
-        // };
+        int[][] arr = {
+            {1, 2, 3},       // 0th index
+            {4, 5},          // 1st index
+            {6, 7, 8, 9}     // 2nd index  -> arr2D[2] = {6, 7, 8, 9}
+        };
         // 2D Array can be of this type also, Hence, Mentioning Columns is not necessary.
 
 
-        int[][] arr = new int [3][3];
-        System.out.println(arr.length);  // no of rows
+        // int[][] arr = new int [3][4];
+        // System.out.println(arr.length);  // no of rows
 
         // input 
 
-        for (int row = 0; row < arr.length; row++){
-            // for each col in every row
-            for (int col = 0; col < arr[row].length; col++){
-                arr[row][col] = in.nextInt();
-            }
-        }
-
-        // output
         // for (int row = 0; row < arr.length; row++){
         //     // for each col in every row
         //     for (int col = 0; col < arr[row].length; col++){
-        //         System.out.print(arr[row][col] + " ");
+        //         arr[row][col] = in.nextInt();
         //     }
-        //     System.out.println();
         // }
+
+        // output
+        for (int row = 0; row < arr.length; row++){
+            // for each col in every row
+            for (int col = 0; col < arr[row].length; col++){
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
 
         // output
         // for (int row = 0; row < arr.length; row++){
@@ -60,9 +58,9 @@ public class MultiDimensionArray {
 
 
         // Using Enhanced For Loop
-        for(int[] a : arr){
-            System.out.println(Arrays.toString(a));
-        }
+        // for(int[] a : arr){
+        //     System.out.println(Arrays.toString(a));
+        // }
     }
 }
 
